98b14efadcd6a7de48c70fa27bc8407b435d1508
sharvarii3 <168175435+sharvarii3@users.noreply.github.com>
2025-04-20 22:08:35 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,27 @@
+// Name: Pooja Ramdas
+// PRN no.: 23070126091
+// Batch: AIML B-1 (2023-2027)
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Main thread started.\n");
+
+        // Creating threads for operations
+        Thread t1 = new Thread(new TaskOne());
+        Thread t2 = new Thread(new TaskTwo());
+
+        // Starting the threads
+        t1.start();
+        t2.start();
+
+        // Ensuring all threads complete
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("\nMain thread finished.");
+    }
+}
